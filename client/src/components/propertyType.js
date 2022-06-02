6c15f1ecc831c29eb16865235e10ecaaebcdbea7
sanbleb_bash <sanbleb9@hotmail.com>
2022-06-02 18:01:47 +0200

    connected strapi backend with cleint

--- client/src/components/propertyType.js
+++ client/src/components/propertyType.js
@@ -1,74 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const PropertyType = () => {
+	const [propertyType, setPropertyType] = useState('');
+
+	useEffect(() => {
+		const fetchProperty = async () => {
+			const { data } = await axios.get(
+				'http://localhost:1337/api/properties?populate=*'
+			);
+			setPropertyType(data);
+		};
+		fetchProperty();
+	}, []);
+
 	return (
 		<div className='h-[300px] z-10 flex items-center'>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
-			<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
-				<img
-					src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
-					alt='placeholder'
-					className='h-3/4 object-cover rounded-md w-[200px]'
-				/>
-				<div>
-					<h1 className='text-gray-800 font-semibold'>hotels</h1>
-					<p className='text-gray-500'>858,567 hotels</p>
-				</div>
-			</div>
+			{propertyType &&
+				propertyType.data.map((property) => (
+					<div className='flex gap-4 h-full p-2 flex-col rounded-md overflow-hidden'>
+						<img
+							src={property.attributes.image}
+							alt='placeholder'
+							className='h-3/4 object-cover rounded-md w-[200px]'
+						/>
+						<div>
+							<h1 className='text-gray-800 font-semibold'>
+								{property.attributes.type}
+							</h1>
+							<p className='text-gray-500'>858,567 hotels</p>
+						</div>
+					</div>
+				))}
 		</div>
 	);
 };
